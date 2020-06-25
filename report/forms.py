fe7b2a22cf4143ff6dd9e341753892cb04ad2cd1
Aditya Kumar Verma <adikv0109@gmail.com>
2020-06-25 22:30:36 +0530

    Put the restriction on date selection

--- report/forms.py
+++ report/forms.py
@@ -2,15 +2,21 @@ from django import forms
 
 from django.contrib.auth.models import User
 from data.models import *
+import datetime
+def present_or_past_date(value):
+    if value > datetime.date.today():
+        raise forms.ValidationError("The date cannot be in the future!")
+    return value
+
 
 class LineChart(forms.Form):
 	username = forms.ModelChoiceField(
 									queryset=User.objects.all().order_by('username'),
 									help_text='select the username')
 	start_date = forms.DateField(
-								widget=forms.widgets.DateInput(attrs={'type': 'date'}))
+								widget=forms.widgets.DateInput(attrs={'type': 'date'}),validators=[present_or_past_date])
 	end_date   = forms.DateField(
-								widget=forms.widgets.DateInput(attrs={'type': 'date'}))
+								widget=forms.widgets.DateInput(attrs={'type': 'date'}),validators=[present_or_past_date])
 	is_csv     = forms.BooleanField(help_text='Download the report as CSV', required=False)
 
 class SampleReport(forms.Form):
@@ -18,10 +24,10 @@ class SampleReport(forms.Form):
 									queryset=User.objects.all().order_by('username'),
 									help_text='select the username')
 	start_date = forms.DateField(
-								widget=forms.widgets.DateInput(attrs={'type': 'date'}),
+								widget=forms.widgets.DateInput(attrs={'type': 'date'}),validators=[present_or_past_date]
 								)
 	end_date   = forms.DateField(
-								widget=forms.widgets.DateInput(attrs={'type': 'date'}),
+								widget=forms.widgets.DateInput(attrs={'type': 'date'}),validators=[present_or_past_date]
 								)
 	is_csv     = forms.BooleanField(help_text='Download the report as CSV', required=False)
 
@@ -36,10 +42,10 @@ class IndustrySales(forms.Form):
 									queryset=User.objects.all().order_by('username'),
 									help_text='select the username')
 	start_date = forms.DateField(
-								widget=forms.widgets.DateInput(attrs={'type': 'date'}),
+								widget=forms.widgets.DateInput(attrs={'type': 'date'}),validators=[present_or_past_date],
 								required = True)
 	end_date   = forms.DateField(
-								widget=forms.widgets.DateInput(attrs={'type': 'date'}),
+								widget=forms.widgets.DateInput(attrs={'type': 'date'}),validators=[present_or_past_date],
 								required = True)
 	is_csv     = forms.BooleanField(help_text='Download the report as CSV', required=False)
 
@@ -49,10 +55,10 @@ class ItemSales(forms.Form):
 									help_text='select the username')
 
 	start_date = forms.DateField(
-								widget=forms.widgets.DateInput(attrs={'type': 'date'}),
+								widget=forms.widgets.DateInput(attrs={'type': 'date'}),validators=[present_or_past_date]
 								)
 	end_date   = forms.DateField(
-								widget=forms.widgets.DateInput(attrs={'type': 'date'}),
+								widget=forms.widgets.DateInput(attrs={'type': 'date'}),validators=[present_or_past_date]
 								)
 	is_csv     = forms.BooleanField(help_text='Download the report as CSV', required=False)
 
@@ -62,10 +68,10 @@ class OutstandingReport(forms.Form):
 									help_text='select the username')
 
 	start_date = forms.DateField(
-								widget=forms.widgets.DateInput(attrs={'type': 'date'}),
+								widget=forms.widgets.DateInput(attrs={'type': 'date'}),validators=[present_or_past_date]
 								)
 	end_date   = forms.DateField(
-								widget=forms.widgets.DateInput(attrs={'type': 'date'}),
+								widget=forms.widgets.DateInput(attrs={'type': 'date'}),validators=[present_or_past_date]
 								)
 	is_csv     = forms.BooleanField(help_text='Download the report as CSV', required=False)
 
@@ -75,10 +81,10 @@ class PaymentRegister(forms.Form):
 									help_text='select the username')
 
 	start_date = forms.DateField(
-								widget=forms.widgets.DateInput(attrs={'type': 'date'}),
+								widget=forms.widgets.DateInput(attrs={'type': 'date'}),validators=[present_or_past_date]
 								)
 	end_date   = forms.DateField(
-								widget=forms.widgets.DateInput(attrs={'type': 'date'}),
+								widget=forms.widgets.DateInput(attrs={'type': 'date'}),validators=[present_or_past_date]
 								)
 	is_csv     = forms.BooleanField(help_text='Download the report as CSV', required=False)
 
@@ -88,10 +94,10 @@ class DailySales(forms.Form):
 									help_text='select the username')
 
 	start_date = forms.DateField(
-								widget=forms.widgets.DateInput(attrs={'type': 'date'}),
+								widget=forms.widgets.DateInput(attrs={'type': 'date'}),validators=[present_or_past_date]
 								)
 	end_date   = forms.DateField(
-								widget=forms.widgets.DateInput(attrs={'type': 'date'}),
+								widget=forms.widgets.DateInput(attrs={'type': 'date'}),validators=[present_or_past_date]
 								)
 	is_csv     = forms.BooleanField(help_text='Download the report as CSV', required=False)
 
@@ -101,10 +107,10 @@ class PerfReport(forms.Form):
 									help_text='select the username')
 
 	start_date = forms.DateField(
-								widget=forms.widgets.DateInput(attrs={'type': 'date'}),
+								widget=forms.widgets.DateInput(attrs={'type': 'date'}),validators=[present_or_past_date]
 								)
 	end_date   = forms.DateField(
-								widget=forms.widgets.DateInput(attrs={'type': 'date'}),
+								widget=forms.widgets.DateInput(attrs={'type': 'date'}),validators=[present_or_past_date]
 								)
 	is_csv     = forms.BooleanField(help_text='Download the report as CSV', required=False)
 
@@ -115,3 +121,4 @@ class StrategicReport(forms.Form):
 
 	year= forms.IntegerField(max_value=2050, min_value=2000,initial=2019)
 	is_csv     = forms.BooleanField(help_text='Download the report as CSV', required=False)
+
